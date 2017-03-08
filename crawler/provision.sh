e3aa22787293fb7d4cef787c66862e4dd0a9e6d1
Assil Ksiksi <cyph0nik@gmail.com>
2017-03-07 23:20:24 -0500

    Do not add Solr to PATH

--- crawler/provision.sh
+++ crawler/provision.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 
-touch ~/.bash_profile
-
 # Update Ubuntu packages
 sudo apt-get -y update
-sudo apt-get -y upgrade
 
 # Install Solr
 ## 1. Install JRE 8x
+sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get -y install openjdk-8-jre
 
 ## 2. Set JAVA_HOME
-sed -i '$ a JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/"' /etc/environment
+sudo sed -i '$ a JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/"' /etc/environment
 source /etc/environment
 
 ## 3. Install Solr 6.4.2
 mkdir -p ~/tools && cd ~/tools
 wget http://archive.apache.org/dist/lucene/solr/6.4.2/solr-6.4.2.tgz
-tar -xvfz solr-6.4.2.tgz
+tar -xvf solr-6.4.2.tgz
 mv solr-6.4.2 solr
 
-## 4. Add Solr to $PATH
-echo 'export PATH="~/tools/solr/bin/:$PATH"' >> ~/.bash_profile
-source ~/.bash_profile
+## 4. Start Solr server on port 9000
+cd solr && bin/solr start -p 9000
 
 # Clone GH repo locally
+cd ~/tools
 sudo apt-get -y install git
 git clone https://github.com/srashed3gatech/mlcrawler6262.git
 
