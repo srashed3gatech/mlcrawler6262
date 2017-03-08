f22a924246d052c04b53c0a7aebf7169550bd33b
Assil Ksiksi <cyph0nik@gmail.com>
2017-03-08 13:10:42 -0500

    Settings indent error

--- crawler/crawler-scrapy/alexatop/alexatop/settings.py
+++ crawler/crawler-scrapy/alexatop/alexatop/settings.py
@@ -66,9 +66,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-   'alexatop.pipelines.AlexaPipeline': 300,
-# }
+ITEM_PIPELINES = {
+    'alexatop.pipelines.AlexaPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
