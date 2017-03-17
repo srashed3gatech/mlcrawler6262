c04811e3fe0cde0b1a2efa9e9872196b2c7f7700
srashed3gatech <mamun028@lawn-128-61-25-25.lawn.gatech.edu>
2017-03-16 20:28:10 -0400

    some useful command and tricks

--- resources/useful_commands.sh
+++ resources/useful_commands.sh
@@ -12,6 +12,12 @@ sudo pip install Twisted==16.4.1
 #how to push json file into solr
 java -Durl="http://localhost:8983/solr/crawler/update" -Dtype=application/json -jar /opt/solr/example/exampledocs/post.jar /opt/crawler/crawler/crawler-scrapy/alexatop/output/results-2017-03-09T22-17-39.json
 
+#run scrapy in background and note pid in run.pid
+nohup scrapy crawl alexa > /dev/null 2>&1 & echo $! > run.pid
+#see log of scrapy
+tail -f /opt/crawler/crawler/crawler-scrapy/alexatop/logs/`ls -1t /opt/crawler/crawler/crawler-scrapy/alexatop/logs/ | head -1`
+
+
 ## configuring dnsmasq for dns caching
 sudo vim /etc/NetworkManager/dnsmasq.d/cache
 #write following on the cache file
@@ -20,3 +26,9 @@ cache-size=1000
 sudo restart network-manager
 #check whats going on - should see what dns server its using
 sudo zgrep dnsmasq /var/log/syslog* | grep dnsmasq
+
+#how to run scrapy into backend while you logged off
+#http://askubuntu.com/questions/8653/how-to-keep-processes-running-after-ending-ssh-session
+#source srcenv
+#http://askubuntu.com/questions/8653/how-to-keep-processes-running-after-ending-ssh-session
+#/opt/crawler/crawler/crawler-scrapy/alexatop$ scrapy crawl alexa ; echo $! > scrapy.pid &
