cfd6202958027ddccff477e868b9be6c2db05d67
Assil Ksiksi <cyph0nik@gmail.com>
2017-03-22 14:10:13 -0400

    Change to parser script

--- crawler/blacklist/blacklist_solr.sh
+++ crawler/blacklist/blacklist_solr.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
 # Simple script that runs blacklist parser and writes data to a Solr collection
-BLACKLIST_CORE = blacklist
-
 python blacklist_parser.py
-
-~/bin/post -c $BLACKLIST_CORE data.json
+~/bin/post -c blacklist data.json
