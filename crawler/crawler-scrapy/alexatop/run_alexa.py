8fd340123ab185958cc59d5b1b3cba9ac41311d9
Assil Ksiksi <cyph0nik@gmail.com>
2017-04-01 19:32:33 -0400

    Trying subprocess approach

--- crawler/crawler-scrapy/alexatop/run_alexa.py
+++ crawler/crawler-scrapy/alexatop/run_alexa.py
@@ -1,6 +1,6 @@
-from scrapy.crawler import CrawlerRunner
+import subprocess
+
 from scrapy.utils.project import get_project_settings
-from twisted.internet import reactor
 
 import alexatop.util as util
 from alexatop.spiders.alexa_spider import AlexaSpider
@@ -10,18 +10,11 @@ CRAWL_NUM = int(100e3)
 
 # Grab Alexa top 1M
 sites = util.grab_alexa(ALEXA_MAX)
-stop = False
 
 for start in range(0, ALEXA_MAX, CRAWL_NUM):
-    # Generate new log file for run
-    settings = get_project_settings()
-    runner = CrawlerRunner(settings)
-
     # Get start URLs for current run
     start_urls = sites[start:start+CRAWL_NUM]
 
     # Start the spider and block until completed
-    d = runner.crawl(AlexaSpider, urls=start_urls)
-    d.addBoth(lambda _: reactor.stop())
-
-    reactor.run()
+    crawl_args = 'urls={0}'.format(start_urls)
+    subprocess.run(['scrapy', 'crawl', '-a', crawl_args])
