50f03cde7bed0dfec206c5ed9c2a1a4e239caff1
srashed3gatech <mamun028@ipsec-143-215-24-152.vpn.gatech.edu>
2017-04-05 20:31:19 -0400

    no tmux - run directly through shellscript

--- resources/run_alexa_crawl.sh
+++ resources/run_alexa_crawl.sh
@@ -4,20 +4,23 @@
 #   or you can experience problems
 /bin/sleep 5
 # Ensure the environment is available
-source /home/USERNAME/.bashrc
+#source /home/crawler/.bashrc
 # There has to be tmux session named alexa
 # if not (first time) create one: /usr/bin/tmux new-session -d -s alexa
 # and detach dont exit/logout by [ctrl+b, d]
 # to check tmux open session: tmux ls
-/usr/bin/tmux a -t alexa
+#/usr/bin/tmux a -t alexa
 # ...and control the tmux session (initially ensure the environment
 #   is available, then run commands)
-/usr/bin/tmux send-keys -t alexa ". ~/.venv/main/bin/activate" C-m
-/usr/bin/tmux send-keys -t alexa "cd ~/mlcrawler6262/crawler/crawler-scrapy/alexatop/" C-m
-/usr/bin/tmux send-keys -t alexa "python run_alexa.py > ./run_log.log 2>&1 & echo $! > run.pid" C-m
+#/usr/bin/tmux send-keys -t alexa ". ~/.venv/main/bin/activate" C-m
+#/usr/bin/tmux send-keys -t alexa "cd ~/mlcrawler6262/crawler/crawler-scrapy/alexatop/" C-m
+#/usr/bin/tmux send-keys -t alexa "python run_alexa.py > ./run_log.log 2>&1 & echo $! > run.pid" C-m
 
-#. ~/.venv/main/bin/activate
-#cd ~/mlcrawler6262/crawler/crawler-scrapy/alexatop/
+. ~/.venv/main/bin/activate
+/bin/sleep 5
+cd ~/mlcrawler6262/crawler/crawler-scrapy/alexatop/
 #python -V
 #python ~/test_me.py
 #scrapy crawl alexa
+cd ~/mlcrawler6262/crawler/crawler-scrapy/alexatop/
+nohup python run_alexa.py > ./run_log.log 2>&1 & echo $! > run.pid
