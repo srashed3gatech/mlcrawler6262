d81046809a180a49c44e0e6e42812fa6c499c6d6
Assil Ksiksi <cyph0nik@gmail.com>
2017-04-13 23:34:36 -0400

    Match date formatting to that of crawler results

--- resources/pbl_crawl.sh
+++ resources/pbl_crawl.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 # Grab blacklist text files for future reference
-mkdir -p pbl_daily
-cd pbl_daily
+cd ~/pbl_daily
 
-curl -o psh_$(date '+%Y-%m-%d').txt "https://hosts-file.net/psh.txt"
-curl -o exp_$(date '+%Y-%m-%d').txt "https://hosts-file.net/exp.txt"
-curl -o emd_$(date '+%Y-%m-%d').txt "https://hosts-file.net/emd.txt"
-curl -o fsa_$(date '+%Y-%m-%d').txt "https://hosts-file.net/fsa.txt"
-curl -o domains_$(date '+%Y-%m-%d').txt "http://mirror2.malwaredomains.com/files/domains.txt"
-curl -o suspiciousdomains_$(date '+%Y-%m-%d').txt "https://isc.sans.edu/feeds/suspiciousdomains_Medium.txt"
+curl -o psh_$(date '+%d-%m-%y').txt "https://hosts-file.net/psh.txt"
+curl -o exp_$(date '+%d-%m-%y').txt "https://hosts-file.net/exp.txt"
+curl -o emd_$(date '+%d-%m-%y').txt "https://hosts-file.net/emd.txt"
+curl -o fsa_$(date '+%d-%m-%y').txt "https://hosts-file.net/fsa.txt"
+curl -o domains_$(date '+%d-%m-%y').txt "http://mirror2.malwaredomains.com/files/domains.txt"
+curl -o suspiciousdomains_$(date '+%d-%m-%y').txt "https://isc.sans.edu/feeds/suspiciousdomains_Medium.txt"
 
 # Parse them into a single list for easier use
-# Filename: blacklist-yyyy-mm-dd.csv
-python3 ../../crawler/blacklist/blacklist_parser.py *.txt
-
-cd ..
+# Filename: blacklist-dd-mm-yy.csv
+python3 /home/crawler/mlcrawler6262/crawler/blacklist/blacklist_parser.py *_$(date '+%d-%m-%y').txt
