d6624cf527cc68f0d4de3006c256429dbf1baf04
Assil Ksiksi <cyph0nik@gmail.com>
2017-04-14 22:27:40 -0400

    First version of data extraction script

---
+++ analysis/extract_data.py
@@ -0,0 +1,12 @@
+'''
+    Extracts data from a given results .json file
+
+    The goal of this script is to extract N website results
+    and summarize their data for insertion into a database (e.g., Solr).
+'''
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
