302bce73b35328ccb83b7b6ba5a2fd1b21bbe898
Assil Ksiksi <cyph0nik@gmail.com>
2017-04-16 15:56:00 -0400

    Implemented lookup table and crawl index classes
    - Lookup table can be built dynamically
    - Crawl index allows for faster rank lookup within a set of files for a day

--- analysis/blacklist_check.py
+++ analysis/blacklist_check.py
@@ -46,6 +46,89 @@ LOOKUP_DIR = '/home/crawler/mlcrawler6262/analysis/lookup'
 os.makedirs(LOOKUP_DIR, exist_ok=True)
 LOOKUP_TABLE = os.path.join(LOOKUP_DIR, 'urls-{0}')
 
+CRAWL_INDEX_DIR = '/home/crawler/mlcrawler6262/crawler/crawler-scrapy/alexatop/data/crawl_index'
+os.makedirs(CRAWL_INDEX_DIR, exist_ok=True)
+CRAWL_INDEX_PATH = os.path.join(CRAWL_INDEX_DIR, 'index-{0}')
+
+class LookupTable:
+    '''
+        Builds and stores a lookup table for crawled URLs for a given day.
+
+        Params: day (dd-mm-yy), idx (int): number of chars of URL to index
+    '''
+    def __init__(self, day, idx=5):
+        self.day = day
+        self.idx = idx
+        self.path = LOOKUP_TABLE.format(self.day)
+        self.table = {}
+
+    def lookup(self, url):
+        results = self.table.get(url[:self.idx], [])
+
+        if results:
+            for u, rank in results:
+                if u == url:
+                    return [url, rank]
+
+        return []
+
+    def insert(self, url, rank):
+        '''Insert an entry into the table.'''
+        lookup = self.table.get(url[:self.idx], [])
+        lookup.append([url, rank])
+        self.table[url[:self.idx]] = lookup
+
+    def dump(self):
+        '''Dumps lookup table to disk.'''
+        with open(self.path, 'wb') as f:
+            pickle.dump(f)
+
+    def load(self):
+        '''Loads lookup table from disk.'''
+        if os.path.exists(self.path):
+            with open(self.path, 'rb') as f:
+                self.table = pickle.load(f)
+        else:
+            self.table = {}
+
+class CrawlIndex:
+    '''
+        Builds a rank index for a given day.
+
+        Given a rank, stores the path and seekpos in the index.
+    '''
+    def __init__(self, day):
+        self.day = day
+        self.path = CRAWL_INDEX_PATH.format(day)
+        self.index = {}
+
+    def insert(self, rank, path, seekpos):
+        self.index[rank] = [path, seekpos]
+
+    def lookup(self, rank):
+        '''Return data for particular rank.'''
+        path, seekpos = self.index.get(rank, [])
+
+        if result:
+            with open(path, 'r') as f:
+                f.seek(seekpos)
+                line = f.readline().strip()
+                return json.parses(line)
+
+        return {}
+
+    def dump(self):
+        with open(self.path, 'wb') as f:
+            pickle.dump(f)
+
+    def load(self):
+        '''Loads index from disk.'''
+        if os.path.exists(self.path):
+            with open(self.path, 'rb') as f:
+                self.index = pickle.load(f)
+        else:
+            self.index = {}
+
 def parse_url(url, p=URL_REGEX):
     return re.search(p, url).group(2)
 
@@ -81,8 +164,8 @@ def build_lookup_table(day):
         Return: True on success, False on failure
     '''
     # If already exists, return
-    if os.path.exists(LOOKUP_TABLE.format(day)):
-        return True
+    # if os.path.exists(LOOKUP_TABLE.format(day)):
+    #     return True
 
     # Get all crawl JSON files for day
     files = sorted([each for each in os.listdir(CRAWL_DATA_DIR) if day in each])
@@ -94,18 +177,30 @@ def build_lookup_table(day):
 
     # Build a hash-based lookup table
     # Key: first 5 chars of URL, Value: list of URLs
-    crawled = {}
+    # crawled = {}
 
     # Track rank offset of current data file
     rank_offset = 0
 
+    lookup_table = LookupTable(day)
+    crawl_index = CrawlIndex(day)
+
     for i, each in enumerate(files):
         path = os.path.join(CRAWL_DATA_DIR, each)
+
+        # Tracks seek position in current file
+        seek_offset = 0
 
         with open(path, 'r') as f:
             for line in f:
                 # Parse each line into a JSON object
                 r = json.loads(line, encoding='utf-8')
+
+                true_rank = r['alexa_rank'] + rank_offset
+
+                # Insert seek data, and update seek position
+                crawl_index.insert(true_rank, path, seek_offset)
+                seek_offset += len(line)
 
                 try:
                     # Extract URL using regex
@@ -114,9 +209,10 @@ def build_lookup_table(day):
                     # Store URL in lookup table
                     # Note: table WILL have dupes (e.g., YouTube)
                     # Kept them to maintain rank data
-                    lookup = crawled.get(url[:5], [])
-                    lookup.append([url, r['alexa_rank'] + rank_offset])
-                    crawled[url[:5]] = lookup
+                    lookup_table.insert(url, true_rank)
+                    # lookup = crawled.get(url[:5], [])
+                    # lookup.append([url, r['alexa_rank'] + rank_offset])
+                    # crawled[url[:5]] = lookup
                 except:
                     # Skips malformed URLs
                     pass
@@ -126,8 +222,12 @@ def build_lookup_table(day):
         print('Completed file ' + str(i+1))
 
     # Save lookup table for later use
-    with open(LOOKUP_TABLE.format(day), 'wb') as f:
-        pickle.dump(crawled, f)
+    # with open(LOOKUP_TABLE.format(day), 'wb') as f:
+    #     pickle.dump(crawled, f)
+
+    # Dump lookup table and crawl index
+    lookup_table.dump()
+    crawl_index.dump()
 
     return True
 
@@ -171,17 +271,19 @@ def check_blacklist(day1, day2):
         print('Found: {0} at rank {1}'.format(url, rank))
 
 def main():
+    build_lookup_table('15-04-17')
+
     # Start from most recent data backwards
-    days = DAYS_CRAWLED[::-1]
-
-    for i, day in enumerate(days):
-        if i == len(DAYS_CRAWLED)-1:
-            break
-
-        day1, day2 = days[i+1], day
-        print('Days: {0} vs. {1}'.format(day2, day1))
-
-        check_blacklist(day1, day2)
+    # days = DAYS_CRAWLED[::-1]
+    #
+    # for i, day in enumerate(days):
+    #     if i == len(DAYS_CRAWLED)-1:
+    #         break
+    #
+    #     day1, day2 = days[i+1], day
+    #     print('Days: {0} vs. {1}'.format(day2, day1))
+    #
+    #     check_blacklist(day1, day2)
 
 if __name__ == '__main__':
     main()
