702ecd33d7f7878f0dcdd1778ea30e1c4160ff07
srashed3gatech <srashed3gatech@gmail.com>
2017-04-17 17:05:34 -0400

    crawl trend graph

--- resources/crawler_stat.py
+++ resources/crawler_stat.py
@@ -3,46 +3,27 @@ import json
 import pandas as pd
 import numpy as np
 import re
-file_path = '/home/crawler/mlcrawler6262/crawler/crawler-scrapy/alexatop/data/'
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+file_path = '/home/crawler/mlcrawler6262/crawler/crawler-scrapy/alexatop/data/stats/'
 
 
-listOfStatsPerFile = list()
+df_allStats = pd.DataFrame()
 
 for filename in os.listdir(file_path):
-    file_date = ""
-    total_line = 0
-    dnsfail = 0
-    httpfail = 0
-    totalerror = 0
-    totalok = 0
+    if filename.startswith("stats-"):
+        file_date = re.findall('stats-([\d\-]+)',filename)[0]
+        statfile = os.path.join(file_path, filename)
+        df = pd.read_csv(statfile)
+        df['date'] = pd.to_datetime(file_date,format='%d-%m-%y')
+        df_allStats = pd.concat([df, df_allStats])
 
-    if filename.startswith("crawldata-"):
-        file_date = re.findall('crawldata-([\d\-]+).json',filename)[0]
-        line_chunk=100
-        with open(os.path.join(file_path, filename), 'r') as f:
-            for line in f:
-                if line_chunk > 0:
-                    line_chunk -= 1
-                else:
-                    break;
-                try:
-                    item = json.loads(line, encoding='utf-8')
-
-                    if 'pk' in item and item['pk']:
-                        total_line += 1
-
-                        if item['crawl_status'] == "OK":
-                            totalok += 1
-                        else:
-                            totalerror += 1
-                            if item['crawl_status'] == "HTTPError":
-                                dnsfail += 1
-                            if item['crawl_status'] == "DNSLookupError":
-                                httpfail += 1
-                except:
-                    pass
-        listOfStatsPerFile.append([file_date, total_line, dnsfail, httpfail, totalerror, totalok,
-                            startAlexaRank, endAlexaRank])
-        print([file_date, total_line, dnsfail, httpfail, totalerror, totalok])
-df = pd.DataFrame(listOfStatsPerFile,columns=['file_date', 'total_line', 'dnsfail',
-            'httpfail', 'totalerror', 'totalok'])
+df_allStats = df_allStats.set_index(['date'])
+f = plt.figure()
+plt.title('Daily Crawl Trend', color='black')
+df_allStats.plot(kind='line', ax=f.gca(), logy=True)
+ax = f.gca()
+ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+plt.show()
