8546e59989247dcee1d10d6feceb1ddba22ef62a
srashed3gatech <srashed3gatech@gmail.com>
2017-04-17 20:13:19 -0400

    roc generation code

---
+++ ml_poc/ml_classifier_roc.py
@@ -0,0 +1,95 @@
+from __future__ import print_function
+
+# ROC generation: http://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import cycle
+import pandas as pd
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from scipy import interp
+
+
+
+df_network_features = pd.read_csv("test_dataset.csv")
+
+del df_network_features['Unnamed: 0'] #delete the index columns
+del df_network_features['url'] #delete the url column - this isn't going into classifier
+#del df_network_features['url_decency']
+df_network_features = df_network_features.sample(frac=1).reset_index(drop=True)
+df_network_features
+
+#Decision Tree Classifier: Generates a decision Tree for the Model
+def encode_target(df, target_column):
+    """Add column to df with integers for the target.
+
+    Args
+    ----
+    df -- pandas DataFrame.
+    target_column -- column to map to int, producing
+                     new Target column.
+
+    Returns
+    -------
+    df_mod -- modified DataFrame.
+    targets -- list of target names.
+    """
+    df_mod = df.copy()
+    targets = df_mod[target_column].unique()
+    map_to_int = {name: n+1 for n, name in enumerate(targets)}
+    df_mod["Target"] = df_mod[target_column].replace(map_to_int)
+
+    return (df_mod, targets)
+
+df2, targets = encode_target(df_network_features, "label")
+features = list(df2.columns[:10])
+
+#we have data already formed in X and y before in Decision Tree - using the same data
+X = df2[features].values
+y = df2["Target"].values
+
+# Binarize the output
+y = label_binarize(y, classes=[1, 2])
+n_classes = y.shape[1]
+
+# Add noisy features to make the problem harder
+random_state = np.random.RandomState(99)
+n_samples, n_features = X.shape
+X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
+
+# shuffle and split training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25,
+                                                    random_state=0)
+
+# Learn to predict each class against the other
+classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True,
+                                 random_state=random_state))
+y_score = classifier.fit(X_train, y_train).decision_function(X_test)
+
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+# Generate the ROC plot
+plt.figure()
+lw = 2
+plt.plot(fpr[0], tpr[0], color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[0])
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.show()
